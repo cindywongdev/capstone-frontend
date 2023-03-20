bf3e0ebce23be5cbe61b5fb224e9ad45cd061d90
Cindy Wong <cindywongdev@gmail.com>
2023-03-20 16:28:20 -0400

    created listing card component

---
+++ src/components/ListingCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="
+        border-4
+        border-red-400
+        container
+        mx-auto
+    ">
+        <h1 class="
+            border-4
+            border-blue-500
+            w-max
+            mx-auto
+        ">{{ listing.food_name }}</h1>
+
+
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    listing: Object
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/AllListings.vue
+++ src/pages/AllListings.vue
@@ -1,23 +1,22 @@
 <template>
     <div>
         <h1 class="text-amber-400 text-3xl font-bold">All Listings</h1>
-        <ul>
-            <li v-for="listing of listings" :key="listing.id">
-                <h2>{{ listing.food_name }}</h2>
-                <img :src="listing.img" alt="">
-            </li>
-        </ul>
+        <section>
+                <ListingCard v-for="listing of listings" :key="listing.id" :listing="listing"/>
+        </section>
     </div>
 </template>
 
 <script setup>
-    const props = defineProps({
-        listings: Array
-    })
+import ListingCard from '../components/ListingCard.vue'
+
+const props = defineProps({
+    listings: Array
+})
 </script>
 
 <style lang="scss" scoped>
-    img {
-        width: 300px;
-    }
+img {
+    width: 300px;
+}
 </style>
