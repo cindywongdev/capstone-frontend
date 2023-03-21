5ffc2e540ac9f7c122df90e9e86f110929c5984f
Cindy Wong <cindywongdev@gmail.com>
2023-03-21 14:37:12 -0400

    added config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(() => {
+  return {
+    define: {
+      __APP_ENV__: process.env.VITE_API_BASE_URL,
+    },
+  };
+});
