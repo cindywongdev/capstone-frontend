09229813e33abceec390954a3ccce76e3a705ff3
Cindy Wong <cindywongdev@gmail.com>
2023-03-21 14:42:47 -0400

    fixed config bug

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    plugins: [vue()],
     define: {
       __APP_ENV__: process.env.VITE_API_BASE_URL,
-    },
-  };
-});
+    }
+}})
