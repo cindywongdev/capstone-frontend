f5d3c4e0c05edb5f6e48425843c035c3e5fcd580
Cindy Wong <cindywongdev@gmail.com>
2023-03-23 13:50:03 -0400

    started styling show page

--- src/pages/SingleListing.vue
+++ src/pages/SingleListing.vue
@@ -1,28 +1,90 @@
 <template>
     <div class="
-        container 
-        flex
-        flex-col
-        items-center
-        justify-center
-        mx-auto">
+                container 
+                relative
+                flex
+                flex-col
+                items-center
+                justify-center
+                mx-auto">
 
-        <router-link to="/"><button class="bg-green-500 p-2 rounded-md">Back to Listings</button></router-link>
+        <!-- top div for text and options button-->
+        <div class="relative flex justify-between w-full">
+            <div>
+                <h2>{{ listing.restaurant }}</h2>
+                <h1>{{ listing.food_name }}</h1>
+                <h2>{{ listing.listing_date }}</h2>
+            </div>
 
-        <h1>{{ listing.food_name }}</h1>
-        <h2>{{ listing.listing_date }}</h2>
-        <img :src="listing.img" :alt="listing.food_name"/>
-        <h2>{{ listing.ingredients }}</h2>
-        <h2>{{ listing.allergens }}</h2>
-        <h2>{{ listing.good_for_x_days }}</h2>
-        <h2>{{ listing.num_servings }}</h2>
-        <h2>{{ listing.pickup_by_time }}</h2>
-        <h2>{{ listing.restaurant }}</h2>
+            <div>
+                <button class="absolute right-2 top-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-10 h-10">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                    </svg>
+                </button>
 
-        <router-link :to="{ name: 'edit', params: { id: listing.id } }"><button class="bg-green-500 p-2 m-2 rounded-md">Edit Listing</button></router-link>
+                <div role="menubar" class="
+                    absolute
+                    flex
+                    flex-col
+                    gap-2
+                    right-0
+                    top-12
+                    bg-white
+                    p-2
+                    border-4
+                    border-red-400
+                ">
+                    <router-link :to="{ name: 'edit', params: { id: listing.id } }" class="flex justify-between items-center p-2 gap-2">
+                        <button class="font-medium rounded-md">Edit Listing</button>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                        </svg>
 
-        <button v-on:click="deletePost" class="bg-red-500 p-2 rounded-md">Delete Listing</button>
-        
+                    </router-link>
+
+                    <button v-on:click="deletePost" class="text-red-400 font-medium p-2 rounded-md flex gap-2">Delete Listing
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- image -->
+        <img :src="listing.img" :alt="listing.food_name" />
+
+        <!-- second div for rest of text -->
+        <div>
+            <div>
+                <h2>ingredients</h2>
+                <h2>{{ listing.ingredients }}</h2>
+            </div>
+            <div>
+                <h2>allergens</h2>
+                <h2>{{ listing.allergens }}</h2>
+            </div>
+            <div>
+                <h2>still good for</h2>
+                <h2>{{ listing.good_for_x_days }} more days</h2>
+            </div>
+            <div>
+                <h2>servings left</h2>
+                <h2>{{ listing.num_servings }}</h2>
+            </div>
+            <div>
+                <h2>pickup by</h2>
+                <h2>{{ listing.pickup_by_time }}</h2>
+            </div>
+        </div>
+
+        <footer>
+            <router-link to="/"><button class="bg-green-500 p-2 rounded-md">Back to Listings</button></router-link>
+        </footer>
     </div>
 </template>
 
@@ -47,7 +109,7 @@ const { listings, url, getListings } = toRefs(props)
 const listing = listings.value.find((listing) => listing.id == route.params.id)
 console.log(listings.value)
 
-const deletePost = async function() {
+const deletePost = async function () {
     await fetch(url.value + listing.id + "/", {
         method: "delete",
     })
@@ -60,6 +122,4 @@ const deletePost = async function() {
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
