8d2355ea9d87326dd05f59ca509c35c22e133b04
Cindy Wong <cindywongdev@gmail.com>
2023-03-27 17:22:34 -0400

    started landing page, added footer

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -5,11 +5,13 @@
         <img src="../assets/beef-noodle-soup.jpg" alt="a bowl of beef noodle soup"/>
         <h3>Photo by <a href="https://unsplash.com/de/@plhnk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Paul Hanaoka</a> on <a href="https://unsplash.com/photos/_q4AQcGX28A?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
         </h3>
+        <LandingFooter/>
     </div>
 </template>
 
 <script setup>
 import Nav from '../components/Nav.vue';
+import LandingFooter from '../components/LandingFooter.vue'
 </script>
 
 <style lang="scss" scoped>
